b5e904bc1b12335bb61c5d2e77dc705c1cdf3f59
kevin <suokunstar@gmail.com>
2019-08-16 18:08:29 -0400

    Update info.sh

--- info.sh
+++ info.sh
@@ -26,4 +26,7 @@ for src in arm core h264 isp v3d uart pwm emmc pixel vec hdmi dpi ; do \
 	  echo -e "$src:\t$(vcgencmd measure_clock $src)" ; \
 done
 
+echo "\n temperature"
+vcgencmd measure_temp
+
 
